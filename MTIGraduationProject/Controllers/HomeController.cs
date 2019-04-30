42c6b2c2ff8e345cb678e2791ef728cea005b9e5
ibrahim hassan <Ibrahim__Hasan@outlook.com>
2019-05-01 01:08:36 +0200

    Adding rtl.bootstrap & Register student module

--- MTIGraduationProject/Controllers/HomeController.cs
+++ MTIGraduationProject/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MTIGraduationProject.Models;
+using MTIGraduationProject.ViewModels;
 
 namespace MTIGraduationProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MTI_GraduationPartyEntities _mtiGraduationPartyEntities;
+
+        public HomeController()
+        {
+            _mtiGraduationPartyEntities = new MTI_GraduationPartyEntities();
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -20,11 +30,54 @@ namespace MTIGraduationProject.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpGet]
+        public ActionResult RegisterStudent()
         {
-            ViewBag.Message = "Your contact page.";
+            var studentViewModel = new StudentViewModel
+            {
+                Id = 0
+            };
 
+            return View(studentViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegisterStudent(StudentViewModel studentViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(studentViewModel);
+
+            var student = new Student
+            {
+                Id = studentViewModel.Id,
+                Name = studentViewModel.Name,
+                Specialization = studentViewModel.Specialization,
+                TableId = studentViewModel.TableId,
+                BusId = studentViewModel.BusId,
+                Chair1Id = studentViewModel.Chair1Id,
+                Chair2Id = studentViewModel.Chair2Id
+            };
+
+            
+            _mtiGraduationPartyEntities.Students.Add(student);
+            _mtiGraduationPartyEntities.SaveChanges();
+
+            return RedirectToAction("RegisterStudent", "Home");
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult RegisterInvitation()
+        {
             return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult EditStudent(StudentViewModel studentViewModel)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ MTIGraduationProject/ViewModels/StudentViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using MTIGraduationProject.Controllers;
+
+namespace MTIGraduationProject.ViewModels
+{
+    public class StudentViewModel
+    {
+        [Required(ErrorMessage = "هذا الحقل مطلوب")]
+        [Range(1, 99999, ErrorMessage = "رقم الطالب غير صحيح")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="هذا الحقل مطلوب")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "هذا الحقل مطلوب")]
+        public string Specialization { get; set; }
+
+        [Required(ErrorMessage = "هذا الحقل مطلوب")]
+        public int? TableId { get; set; }
+
+        [Required(ErrorMessage = "هذا الحقل مطلوب")]
+        public int? BusId { get; set; }
+
+        [Required(ErrorMessage = "هذا الحقل مطلوب")]
+        public int? Chair1Id { get; set; }
+
+        [Required(ErrorMessage = "هذا الحقل مطلوب")]
+        public int? Chair2Id { get; set; }
+
+        public string Action
+        {
+            get
+            {
+                Expression<Func<HomeController, ActionResult>> edit =
+                    (c => c.EditStudent(this));
+                Expression<Func<HomeController, ActionResult>> create =
+                    (c => c.RegisterStudent(this));
+
+                var action = (Id != 0) ? edit : create;
+
+                return (action.Body as MethodCallExpression)?.Method.Name;
+            }
+        }
+    }
+}
