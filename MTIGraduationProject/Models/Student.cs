6d1da9b0ad07972f7238cee0edc39b6167ab4eba
ibrahim hassan <Ibrahim__Hasan@outlook.com>
2019-05-24 22:16:50 +0200

    Adding System test module

--- MTIGraduationProject/Models/Student.cs
+++ MTIGraduationProject/Models/Student.cs
@@ -18,6 +18,7 @@ namespace MTIGraduationProject.Models
         public Student()
         {
             this.Invitations = new HashSet<Invitation>();
+            this.SystemTests = new HashSet<SystemTest>();
         }
     
         public int Id { get; set; }
@@ -29,5 +30,7 @@ namespace MTIGraduationProject.Models
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Invitation> Invitations { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SystemTest> SystemTests { get; set; }
     }
 }
